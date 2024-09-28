5f2b0be730cb66489b5a30a78e6bebc4984b4f9d
deb2000-sudo <cdnayak2000@gmail.com>
2024-09-28 16:33:03 +0530

    fontfamily as Poppins added

--- src/components/BlogCard/Blogcard.jsx
+++ src/components/BlogCard/Blogcard.jsx
@@ -10,10 +10,10 @@ const Blogcard=()=>{
                 width={1}
             />
             <Box p={2}>
-                <Typography color='#77829D' fontWeight={500} mb={1} fontSize={{ xs: 12, md: 16 }}>
+                <Typography color='#77829D' fontWeight={500} mb={1} fontSize={{ xs: 12, md: 16 }} fontFamily='Poppins'>
                     Medical | March 31, 2022
                 </Typography>
-                <Typography component='h3' color='#1B3C74' fontSize={{ xs: 14, md: 18 }} fontWeight={500} lineHeight={1.2} mb={2}>
+                <Typography component='h3' color='#1B3C74' fontSize={{ xs: 14, md: 18 }} fontWeight={500} lineHeight={1.2} mb={2} fontFamily='Poppins'>
                     6 Tips To Protect Your Mental Health When You're Sick
                 </Typography>
                 <Stack direction='row' spacing={1} alignItems='center'>
@@ -23,7 +23,7 @@ const Blogcard=()=>{
                         height={32}
                         width={32}
                     />
-                    <Typography color='#1B3C74' fontSize={{ xs: 12, md: 16 }}>
+                    <Typography color='#1B3C74' fontSize={{ xs: 12, md: 16 }} fontFamily='Poppins'>
                         Rebecca Lee
                     </Typography>
                 </Stack>
