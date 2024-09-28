45a2b2456c29f3a69e05b71f79f87a85b178c968
deb2000-sudo <cdnayak2000@gmail.com>
2024-09-28 16:31:20 +0530

    Blog section is added

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,11 @@
 import FAQ from "../components/Sections/FAQ/FAQ";
 import {Box} from '@mui/material';
 import Ourfamilies from "../components/Sections/OurFamilies/Ourfamilies";
+import Blogs from "../components/Sections/Blogs/Blogs";
 const Home=()=>{
 return(
     <Box>
+        <Blogs/>
         <Ourfamilies/>
         <FAQ/>
     </Box>
--- src/components/Sections/Blogs/Blogs.jsx
+++ src/components/Sections/Blogs/Blogs.jsx
@@ -1,8 +1,31 @@
 import { Box, Container, Grid, Typography } from "@mui/material";
+import Blogcard from "../../BlogCard/Blogcard";
 const Blogs=()=>{
     return(
+        <Box py={6}>
+            <Container>
+                <Typography color='#2AA7FF' fontWeight={600} textAlign='center'>
+                    Blog & News
+                </Typography>
+                <Typography textAlign='center' variant='h2' mb={2} fontFamily='Poppins' color='#1B3C74' fontWeight={600}>
+                    Read Our Latest News
+                </Typography>
 
-    )
+                <Grid container spacing={4}>
+                    <Grid item xs={12} md={4}>
+                        <Blogcard />
+                    </Grid>
+                    <Grid item xs={12} md={4}>
+                        <Blogcard />
+                    </Grid>
+                    <Grid item xs={12} md={4}>
+                        <Blogcard />
+                    </Grid>
+                </Grid>
+
+            </Container>
+        </Box>
+    );
 }
 
 export default Blogs;
